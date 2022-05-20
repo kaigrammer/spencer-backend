4e24966df790111be1e48c5aad317a6d235e9021
kaigrammer <khairulakmal.work@gmail.com>
2022-05-20 11:44:20 +0800

    feat: add expense endpoints create and show

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -9,7 +9,12 @@ import (
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
 
+	// API health status endpoint
 	router.HandlerFunc(http.MethodGet, "/api/v1/healthcheck", app.healthcheckHandler)
+
+	// Expenses endpoint
+	router.HandlerFunc(http.MethodPost, "/api/v1/expenses", app.createExpenseHandler)
+	router.HandlerFunc(http.MethodGet, "/api/v1/expenses/:id", app.showExpenseHandler)
 
 	return router
 }
