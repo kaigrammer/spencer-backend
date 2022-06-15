f0e5f968c98bcf9891ddb2b7bf3ae06da8ec1414
kaigrammer <khairulakmal.work@gmail.com>
2022-06-15 11:49:47 +0800

    feat: update to use a map for healthcheck response

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "status: available, environment: %s, version: %s\n", app.config.env, VERSION)
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     VERSION,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "the server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
